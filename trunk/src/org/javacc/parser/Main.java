c6246b776684c8979ad63c69c376e702619b5b48
tcopeland <tcopeland@68c0822f-77d1-6f10-b115-8a14d106ca4c>
2006-03-15 18:45:29 +0000

    Just fixing a little typo

--- trunk/src/org/javacc/parser/Main.java
+++ trunk/src/org/javacc/parser/Main.java
@@ -132,7 +132,7 @@ public class Main {
       parser = new JavaCCParser(new java.io.FileReader(args[args.length-1]));
     } catch (NullPointerException ne) { // Should never happen
     } catch (SecurityException se) {
-      System.out.println("Security voilation while trying to open " + args[args.length-1]);
+      System.out.println("Security violation while trying to open " + args[args.length-1]);
       return 1;
     } catch (java.io.FileNotFoundException e) {
       System.out.println("File " + args[args.length-1] + " not found.");
