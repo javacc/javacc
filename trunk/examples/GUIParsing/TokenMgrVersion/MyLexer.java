ef0e833390eba950420bc88e51e1eb7f57ed375f
tcopeland <tcopeland@68c0822f-77d1-6f10-b115-8a14d106ca4c>
2006-07-18 20:50:06 +0000

    A tiny typo

--- trunk/examples/GUIParsing/TokenMgrVersion/MyLexer.java
+++ trunk/examples/GUIParsing/TokenMgrVersion/MyLexer.java
@@ -31,7 +31,7 @@ class MyLexer extends CalcInputParserTokenManager {
 
    /**
     * We redefined the lexical error reporting function so that it beeps
-    * and displays a messgae thru the GUI.
+    * and displays a message thru the GUI.
     */
    protected void LexicalError() {
      CalcGUI.Error("ERROR (click 0)");
