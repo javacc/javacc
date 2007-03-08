ee6f4d24454192805b1025ffbfd52af095ec89c0
timp_work <timp_work@68c0822f-77d1-6f10-b115-8a14d106ca4c>
2007-03-08 14:07:25 +0000

    Remove unused loop labels, * import and whitespace in licence

--- src/org/javacc/parser/RCharacterList.java
+++ src/org/javacc/parser/RCharacterList.java
@@ -1,9 +1,9 @@
 /* Copyright (c) 2006, Sun Microsystems, Inc.
  * All rights reserved.
- * 
+ *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are met:
- * 
+ *
  *     * Redistributions of source code must retain the above copyright notice,
  *       this list of conditions and the following disclaimer.
  *     * Redistributions in binary form must reproduce the above copyright
@@ -12,7 +12,7 @@
  *     * Neither the name of the Sun Microsystems, Inc. nor the names of its
  *       contributors may be used to endorse or promote products derived from
  *       this software without specific prior written permission.
- * 
+ *
  * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
  * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
@@ -25,11 +25,9 @@
  * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF
  * THE POSSIBILITY OF SUCH DAMAGE.
  */
-
 package org.javacc.parser;
 
-import java.util.*;
-
+import java.util.Vector;
 /**
  * Describes character lists.
  */
@@ -176,7 +174,6 @@ static final char[] diffUpperCaseRanges = {
   {
      int cnt = descriptors.size();
 
-     OuterLoop:
      for (int i = 0; i < cnt; i++)
      {
         if (descriptors.elementAt(i) instanceof SingleCharacter)
@@ -526,7 +523,6 @@ static final char[] diffUpperCaseRanges = {
      Vector newDescriptors = new Vector();
      int lastRemoved = -1; // One less than the first valid character.
 
-   OuterLoop:
      for (i = 0; i < descriptors.size(); i++)
      {
         if (descriptors.elementAt(i) instanceof SingleCharacter)
