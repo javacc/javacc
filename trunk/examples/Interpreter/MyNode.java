3b848ca53899ea675f5b700d418cbe9f84329a72
timp_work <timp_work@68c0822f-77d1-6f10-b115-8a14d106ca4c>
2007-04-04 11:33:04 +0000

    Add settable in and out to enable testing

--- trunk/examples/Interpreter/MyNode.java
+++ trunk/examples/Interpreter/MyNode.java
@@ -1,9 +1,9 @@
 /* Copyright (c) 2006, Sreenivasa Viswanadha <sreeni@viswanadha.net>
  * All rights reserved.
- * 
+ *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are met:
- * 
+ *
  *     * Redistributions of source code must retain the above copyright notice,
  *       this list of conditions and the following disclaimer.
  *     * Redistributions in binary form must reproduce the above copyright
@@ -12,7 +12,7 @@
  *     * Neither the name of the Sun Microsystems, Inc. nor the names of its
  *       contributors may be used to endorse or promote products derived from
  *       this software without specific prior written permission.
- * 
+ *
  * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
  * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
@@ -26,6 +26,14 @@
  * THE POSSIBILITY OF SUCH DAMAGE.
  */
 
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.io.Reader;
+import java.io.Writer;
+
+/**
+ * Specialised node.
+ */
 public class MyNode
 {
   /** Symbol table */
@@ -35,8 +43,27 @@ public class MyNode
   protected static Object[] stack = new Object[1024];
   protected static int top = -1;
 
+  /** @throws UnsupportedOperationException if called */
   public void interpret()
   {
      throw new UnsupportedOperationException(); // It better not come here.
   }
+
+  protected static Writer out = new PrintWriter(System.out);
+  protected static Reader in = new InputStreamReader(System.in);
+
+  /**
+   * @param in the input to set
+   */
+  public static void setIn(Reader in) {
+    MyNode.in = in;
+  }
+
+  /**
+   * @param out the output to set
+   */
+  public static void setOut(Writer out) {
+    MyNode.out = out;
+  }
+
 }
