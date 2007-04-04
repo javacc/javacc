f587e88625e1cb2382b42df3b7eebed38a2c256f
timp_work <timp_work@68c0822f-77d1-6f10-b115-8a14d106ca4c>
2007-04-04 14:07:14 +0000

    Windows line ends in header, javadoc

--- examples/JJTreeExamples/ASTMyID.java
+++ examples/JJTreeExamples/ASTMyID.java
@@ -1,9 +1,9 @@
 /* Copyright (c) 2006, Sun Microsystems, Inc.
  * All rights reserved.
- * 
+ *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are met:
- * 
+ *
  *     * Redistributions of source code must retain the above copyright notice,
  *       this list of conditions and the following disclaimer.
  *     * Redistributions in binary form must reproduce the above copyright
@@ -12,7 +12,7 @@
  *     * Neither the name of the Sun Microsystems, Inc. nor the names of its
  *       contributors may be used to endorse or promote products derived from
  *       this software without specific prior written permission.
- * 
+ *
  * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
  * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
@@ -27,17 +27,33 @@
  */
 
 
+/**
+ * An ID.
+ */
 public class ASTMyID extends SimpleNode {
   private String name;
 
-  ASTMyID(int id){
+  /**
+   * Constructor.
+   * @param id the id
+   */
+  public ASTMyID(int id) {
     super(id);
   }
 
+
+  /**
+   * Set the name.
+   * @param n the name
+   */
   public void setName(String n) {
     name = n;
   }
 
+  /**
+   * {@inheritDoc}
+   * @see org.javacc.examples.jjtree.eg2.SimpleNode#toString()
+   */
   public String toString() {
     return "Identifier: " + name;
   }
