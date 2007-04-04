69c60ff91a2c0c030c20a44259fa7d9c674a0661
timp_work <timp_work@68c0822f-77d1-6f10-b115-8a14d106ca4c>
2007-04-04 11:49:22 +0000

    Windows line ends in header, regenerated to make methods public and gain new parser constructor

--- trunk/examples/Interpreter/ASTFalseNode.java
+++ trunk/examples/Interpreter/ASTFalseNode.java
@@ -1,9 +1,9 @@
 /* Copyright (c) 2006, Sun Microsystems, Inc.
  * All rights reserved.
- * 
+ *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are met:
- * 
+ *
  *     * Redistributions of source code must retain the above copyright notice,
  *       this list of conditions and the following disclaimer.
  *     * Redistributions in binary form must reproduce the above copyright
@@ -12,7 +12,7 @@
  *     * Neither the name of the Sun Microsystems, Inc. nor the names of its
  *       contributors may be used to endorse or promote products derived from
  *       this software without specific prior written permission.
- * 
+ *
  * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
  * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
@@ -26,17 +26,16 @@
  * THE POSSIBILITY OF SUCH DAMAGE.
  */
 
-
 /* JJT: 0.2.2 */
 
-
-
-
 public class ASTFalseNode extends SimpleNode {
-  ASTFalseNode(int id) {
+  public ASTFalseNode(int id) {
     super(id);
   }
 
+  public ASTFalseNode(SPLParser p, int id) {
+    super(p, id);
+  }
 
   public void interpret()
   {
