bfc557f857314585d1266e3ce49a2f02d87d0917
paulcager <paulcager@68c0822f-77d1-6f10-b115-8a14d106ca4c>
2007-11-13 22:20:15 +0000

    Back out JJTreeNode change until worked out why it causes bootstrapping to fail

---
+++ trunk/src/org/javacc/jjtree/SimpleNode.java
@@ -0,0 +1,271 @@
+/* Copyright (c) 2006, Sun Microsystems, Inc.
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ *     * Redistributions of source code must retain the above copyright notice,
+ *       this list of conditions and the following disclaimer.
+ *     * Redistributions in binary form must reproduce the above copyright
+ *       notice, this list of conditions and the following disclaimer in the
+ *       documentation and/or other materials provided with the distribution.
+ *     * Neither the name of the Sun Microsystems, Inc. nor the names of its
+ *       contributors may be used to endorse or promote products derived from
+ *       this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF
+ * THE POSSIBILITY OF SUCH DAMAGE.
+ */
+package org.javacc.jjtree;
+
+public class SimpleNode implements Node {
+  private Node parent;
+  private Node[] children;
+  private int id;
+  private int myOrdinal;
+
+  public SimpleNode(int i) {
+    id = i;
+  }
+
+  public SimpleNode(JJTreeParser p, int i) {
+    this(i);
+  }
+
+  public void jjtOpen() {}
+  public void jjtClose() {}
+
+  public void jjtSetParent(Node n) { parent = n; }
+  public Node jjtGetParent() { return parent; }
+
+  public void jjtAddChild(Node n, int i) {
+    if (children == null) {
+      children = new Node[i + 1];
+    } else if (i >= children.length) {
+      Node c[] = new Node[i + 1];
+      System.arraycopy(children, 0, c, 0, children.length);
+      children = c;
+    }
+    children[i] = n;
+    ((SimpleNode)n).setOrdinal(i);
+  }
+
+  public Node jjtGetChild(int i) {
+    return children[i];
+  }
+
+  public int jjtGetNumChildren() {
+    return (children == null) ? 0 : children.length;
+  }
+
+  /* You can override these two methods in subclasses of SimpleNode to
+     customize the way the node appears when the tree is dumped.  If
+     your output uses more than one line you should override
+     toString(String), otherwise overriding toString() is probably all
+     you need to do. */
+
+  public String toString() { return JJTreeParserTreeConstants.jjtNodeName[id]; }
+  public String toString(String prefix) { return prefix + toString(); }
+
+  /* Override this method if you want to customize how the node dumps
+     out its children. */
+
+  public void dump(String prefix) {
+    System.out.println(toString(prefix));
+    if (children != null) {
+      for (int i = 0; i < children.length; ++i) {
+        SimpleNode n = (SimpleNode)children[i];
+        if (n != null) {
+          n.dump(prefix + " ");
+        }
+      }
+    }
+  }
+
+  public int getOrdinal()
+  {
+    return myOrdinal;
+  }
+
+  public void setOrdinal(int o)
+  {
+    myOrdinal = o;
+  }
+
+
+  /*****************************************************************
+   *
+   * The following is added manually to enhance all tree nodes with
+   * attributes that store the first and last tokens corresponding to
+   * each node, as well as to print the tokens back to the specified
+   * output stream.
+   *
+   *****************************************************************/
+
+  private Token first, last;
+
+  public Token getFirstToken() { return first; }
+  public void setFirstToken(Token t) { first = t; }
+  public Token getLastToken() { return last;  }
+  public void setLastToken(Token t) { last = t; }
+
+  /* This method prints the tokens corresponding to this node
+     recursively calling the print methods of its children.
+     Overriding this print method in appropriate nodes gives the
+     output the added stuff not in the input.  */
+
+  public void print(IO io) {
+    /* Some productions do not consume any tokens.  In that case their
+       first and last tokens are a bit strange. */
+    if (getLastToken().next == getFirstToken()) {
+      return;
+    }
+
+    Token t1 = getFirstToken();
+    Token t = new Token();
+    t.next = t1;
+    SimpleNode n;
+    for (int ord = 0; ord < jjtGetNumChildren(); ord++) {
+      n = (SimpleNode)jjtGetChild(ord);
+      while (true) {
+        t = t.next;
+        if (t == n.getFirstToken()) break;
+        print(t, io);
+      }
+      n.print(io);
+      t = n.getLastToken();
+    }
+    while (t != getLastToken()) {
+      t = t.next;
+      print(t, io);
+    }
+  }
+
+
+  String translateImage(Token t)
+  {
+    return t.image;
+  }
+
+  String whiteOut(Token t)
+  {
+    String s = "";
+
+    for (int i = 0; i < t.image.length(); ++i) {
+      s += " ";
+    }
+    return s;
+  }
+
+
+
+  /* Indicates whether the token should be replaced by white space or
+     replaced with the actual node variable. */
+  private boolean whitingOut = false;
+
+  protected void print(Token t, IO io) {
+    Token tt = t.specialToken;
+    if (tt != null) {
+      while (tt.specialToken != null) tt = tt.specialToken;
+      while (tt != null) {
+        io.print(TokenUtils.addUnicodeEscapes(translateImage(tt)));
+        tt = tt.next;
+      }
+    }
+
+    /* If we're within a node scope we modify the source in the
+       following ways:
+
+       1) we rename all references to `jjtThis' to be references to
+       the actual node variable.
+
+       2) we replace all calls to `jjtree.currentNode()' with
+       references to the node variable. */
+
+    NodeScope s = NodeScope.getEnclosingNodeScope(this);
+    if (s == null) {
+      /* Not within a node scope so we don't need to modify the
+         source. */
+      io.print(TokenUtils.addUnicodeEscapes(translateImage(t)));
+      return;
+    }
+
+    if (t.image.equals("jjtThis")) {
+      io.print(s.getNodeVariable());
+      return;
+    } else if (t.image.equals("jjtree")) {
+      if (t.next.image.equals(".")) {
+        if (t.next.next.image.equals("currentNode")) {
+          if (t.next.next.next.image.equals("(")) {
+            if (t.next.next.next.next.image.equals(")")) {
+              /* Found `jjtree.currentNode()' so go into white out
+                 mode.  We'll stay in this mode until we find the
+                 closing parenthesis. */
+              whitingOut = true;
+            }
+          }
+        }
+      }
+    }
+    if (whitingOut) {
+      if (t.image.equals("jjtree")) {
+        io.print(s.getNodeVariable());
+        io.print(" ");
+      } else if (t.image.equals(")")) {
+        io.print(" ");
+        whitingOut = false;
+      } else {
+        for (int i = 0; i < t.image.length(); ++i) {
+          io.print(" ");
+        }
+      }
+      return;
+    }
+
+    io.print(TokenUtils.addUnicodeEscapes(translateImage(t)));
+  }
+
+
+  static void openJJTreeComment(IO io, String arg)
+  {
+    if (arg != null) {
+      io.print("/*@bgen(jjtree) " + arg + " */");
+    } else {
+      io.print("/*@bgen(jjtree)*/");
+    }
+  }
+
+
+  static void closeJJTreeComment(IO io)
+  {
+    io.print("/*@egen*/");
+  }
+
+
+  String getIndentation(SimpleNode n)
+  {
+    return getIndentation(n, 0);
+  }
+
+
+  String getIndentation(SimpleNode n, int offset)
+  {
+    String s = "";
+    for (int i = offset + 1; i < n.getFirstToken().beginColumn; ++i) {
+      s += " ";
+    }
+    return s;
+  }
+
+}
+
+/*end*/
