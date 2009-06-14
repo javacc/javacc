bb422eded8a7424f5fa21e30f8087ec138525635
paulcager <paulcager@68c0822f-77d1-6f10-b115-8a14d106ca4c>
2009-06-14 12:37:37 +0000

    Issue 126 - classpath in .bat files

--- trunk/bin/javacc.bat
+++ trunk/bin/javacc.bat
@@ -1,4 +1,4 @@
 
 @echo off
 
-java -classpath "%~f0\..\lib\javacc.jar" javacc %1 %2 %3 %4 %5 %6 %7 %8 %9
+java -classpath "%~dp0lib\javacc.jar;%~dp0lib\javacc.jar;%~f0\..\lib\javacc.jar" javacc %1 %2 %3 %4 %5 %6 %7 %8 %9
