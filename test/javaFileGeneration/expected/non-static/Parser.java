fed922ceaefbb156861749b60993224f73bbdf88
paulcager <paulcager@68c0822f-77d1-6f10-b115-8a14d106ca4c>
2009-06-23 15:19:28 +0000

    Update tests for new release number / JDK1.5 default

--- test/javaFileGeneration/expected/non-static/Parser.java
+++ test/javaFileGeneration/expected/non-static/Parser.java
@@ -160,7 +160,7 @@ public class Parser implements ParserConstants {
       return (jj_ntk = jj_nt.kind);
   }
 
-  private java.util.List jj_expentries = new java.util.ArrayList();
+  private java.util.List<int[]> jj_expentries = new java.util.ArrayList<int[]>();
   private int[] jj_expentry;
   private int jj_kind = -1;
 
@@ -190,7 +190,7 @@ public class Parser implements ParserConstants {
     }
     int[][] exptokseq = new int[jj_expentries.size()][];
     for (int i = 0; i < jj_expentries.size(); i++) {
-      exptokseq[i] = (int[])jj_expentries.get(i);
+      exptokseq[i] = jj_expentries.get(i);
     }
     return new ParseException(token, exptokseq, tokenImage);
   }
