7fa0429c4e2fd71b3d48eb268cb913cb0e586c7a
sreeni <sreeni@68c0822f-77d1-6f10-b115-8a14d106ca4c>
2011-11-05 05:41:17 +0000

    Fixed copyright/author info

--- trunk/src/org/javacc/jjtree/JJTreeOptions.java
+++ trunk/src/org/javacc/jjtree/JJTreeOptions.java
@@ -1,4 +1,6 @@
 // Copyright 2011 Google Inc. All Rights Reserved.
+// Author: sreeni@google.com (Sreeni Viswanadha)
+
 /* Copyright (c) 2005-2006, Kees Jan Koster kjkoster@kjkoster.org
  * All rights reserved.
  *
