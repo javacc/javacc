b38b764035973f130e30f2b2487433c8d630ca4f
ainsleyc <ainsleyc@68c0822f-77d1-6f10-b115-8a14d106ca4c>
2012-10-22 14:15:47 +0000

    'LEGACY_EXCEPTION_HANDLING' now omits the throwing of Errors/Exceptions when a value is not returned from a method with a return value. This is kind of redundant as modern IDEs and the Java compiler itself will flag this as an error.
    The old behaviour remains when the 'LEGACY_EXCEPTION_HANDLING = true' option is set.
    
    This does not affect CPP code generation.

--- branches/version_61/src/main/java/org/javacc/parser/ParseEngine.java
+++ branches/version_61/src/main/java/org/javacc/parser/ParseEngine.java
@@ -586,8 +586,12 @@ public class ParseEngine {
     dumpFormattedString(code);
     codeGenerator.genCodeLine("");
     if (p.isJumpPatched() && !voidReturn) {
-      if (isJavaDialect) { 
-        codeGenerator.genCodeLine("    throw new "+(Options.isLegacyExceptionHandling() ? "Error" : "RuntimeException")+"(\"Missing return statement in function\");");
+      if (isJavaDialect) {
+    	  
+    	// I don't think we need to throw an Error/Exception to mark that a return statement is missing as the compiler will flag this error automatically
+    	if (Options.isLegacyExceptionHandling()) {
+    		codeGenerator.genCodeLine("    throw new "+(Options.isLegacyExceptionHandling() ? "Error" : "RuntimeException")+"(\"Missing return statement in function\");");
+    	}
       } else {
         codeGenerator.genCodeLine("    throw \"Missing return statement in function\";");
       }
