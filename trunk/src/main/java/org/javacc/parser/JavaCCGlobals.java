1c6867d495771fe84b5754f356e946113b93bd79
ainsleyc <ainsleyc@68c0822f-77d1-6f10-b115-8a14d106ca4c>
2014-03-05 18:01:07 +0000

    Fix for backslashes being generated as escaped unicode strings in the Java generated code
    For example:
    
    String myString = "\\\"";
    
    Was generated as :
    
    String myString = "\u005c\u005c\u005c"";

--- trunk/src/main/java/org/javacc/parser/JavaCCGlobals.java
+++ trunk/src/main/java/org/javacc/parser/JavaCCGlobals.java
@@ -393,7 +393,7 @@ public class JavaCCGlobals {
 	    char ch;
 	    for (int i = 0; i < str.length(); i++) {
 	      ch = str.charAt(i);
-	      if (ch < 0x20 || ch > 0x7e || ch == '\\') {
+	      if (ch < 0x20 || ch > 0x7e /*|| ch == '\\' -- cba commented out 20140305*/ ) {
 	        String s = "0000" + Integer.toString(ch, 16);
 	        retval += "\\u" + s.substring(s.length() - 4, s.length());
 	      } else {
