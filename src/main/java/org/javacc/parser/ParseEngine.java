da4eb453d390955a68cd82214142272c33700d8c
Philip Helger <philip@helger.com>
2017-08-31 10:01:15 +0200

    Update ParseEngine.java
    Proposed fix for #28

--- src/main/java/org/javacc/parser/ParseEngine.java
+++ src/main/java/org/javacc/parser/ParseEngine.java
@@ -706,10 +706,8 @@ public class ParseEngine {
     
     if (p.isJumpPatched() && !voidReturn) {
       if (isJavaDialect) {
-    	// TODO :: I don't think we need to throw an Error/Exception to mark that a return statement is missing as the compiler will flag this error automatically
-    	if (Options.isLegacyExceptionHandling()) {
-    		codeGenerator.genCodeLine("    throw new "+(Options.isLegacyExceptionHandling() ? "Error" : "RuntimeException")+"(\"Missing return statement in function\");");
-    	}
+	// This line is required for Java!
+	codeGenerator.genCodeLine("    throw new "+(Options.isLegacyExceptionHandling() ? "Error" : "RuntimeException")+"(\"Missing return statement in function\");");
       } else {
         codeGenerator.genCodeLine("    throw \"Missing return statement in function\";");
       }
