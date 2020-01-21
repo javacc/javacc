f2aa352169d0df8ddf5883415b58f5e627bc2574
Francis ANDRE <francis.andre.kampbell@orange.fr>
2020-01-21 09:05:14 +0100

    remove CODE_GENERATOR implicit option

--- javacc.bat
+++ javacc.bat
@@ -5,5 +5,5 @@ set CPP=%M2%\codegen\cpp\8.0.0\cpp-8.0.0.jar
 set JAVA=%M2%\codegen\java\8.0.0\java-8.0.0.jar
 set CSHARP=%M2%\codegen\csharp\8.0.0\csharp-8.0.0.jar
 set CP=%PARSER%;%JAVA%;%CPP%
-java -cp %CP% javacc -CODE_GENERATOR=Java %*
+java -cp %CP% javacc %*
 endlocal
