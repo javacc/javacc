1ad813ec99c9fd8911fdd8af805cadafbf20e0fd
Francis ANDRE <francis.andre.kampbell@orange.fr>
2020-01-21 09:05:47 +0100

    Change hard coded path to C:\Users\FrancisANDRE

--- jjtree.bat
+++ jjtree.bat
@@ -1,5 +1,5 @@
 setlocal
-set M2=C:\Users\fandre\.m2\repository\org\javacc
+set M2=%USERPROFILE%\.m2\repository\org\javacc
 set PARSER=%M2%\parser\8.0.0\parser-8.0.0.jar
 set CPP=%M2%\codegen\cpp\8.0.0\cpp-8.0.0.jar
 set JAVA=%M2%\codegen\java\8.0.0\java-8.0.0.jar
