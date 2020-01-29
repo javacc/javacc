bbae3087c141c846251e4320b36cc16fff15ddee
Francis ANDRE <francis.andre.kampbell@orange.fr>
2020-01-29 14:38:21 +0100

    Adding VS2017 solution

---
+++ examples/cpp/JJTreeExamples/eg1/vs2017/EG1/EG1/pch.h
@@ -0,0 +1,14 @@
+// Conseils pour bien démarrer : 
+//   1. Utilisez la fenêtre Explorateur de solutions pour ajouter des fichiers et les gérer.
+//   2. Utilisez la fenêtre Team Explorer pour vous connecter au contrôle de code source.
+//   3. Utilisez la fenêtre Sortie pour voir la sortie de la génération et d'autres messages.
+//   4. Utilisez la fenêtre Liste d'erreurs pour voir les erreurs.
+//   5. Accédez à Projet > Ajouter un nouvel élément pour créer des fichiers de code, ou à Projet > Ajouter un élément existant pour ajouter des fichiers de code existants au projet.
+//   6. Pour rouvrir ce projet plus tard, accédez à Fichier > Ouvrir > Projet et sélectionnez le fichier .sln.
+
+#ifndef PCH_H
+#define PCH_H
+
+// TODO: Ajoutez les en-têtes à précompiler ici
+
+#endif //PCH_H
